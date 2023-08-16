ed8bd7d879faad174abd34d48b44f8518169326d
P Nishanth Reddy <119839756+nishanth-reddy-2005@users.noreply.github.com>
2023-08-16 14:35:03 +0530

    import java.util.*; public class Nis{     public static void main(String[] args)     {         Scanner sn=new Scanner(System.in);         String a;         a=sn.nextLine();         System.out.print(a);     } }

---
+++ Take_Input_And_Print.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Nis{
+    public static void main(String[] args)
+    {
+        Scanner sn=new Scanner(System.in);
+        String a;
+        a=sn.nextLine();
+        System.out.print(a);
+    }
+}
