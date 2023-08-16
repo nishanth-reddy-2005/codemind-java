77d2bf08b40661be38013b739f0d9edc5864c659
P Nishanth Reddy <119839756+nishanth-reddy-2005@users.noreply.github.com>
2023-08-16 14:22:45 +0530

    import java.util.*; public class Nis{     public static void main(String[] args)     {         Scanner n=new Scanner(System.in);         float a,b,c;         a=n.nextFloat();         b=n.nextFloat();         c=(a*b);         System.out.printf("%.2f",c);     } }

---
+++ Program_to_Multiply_Two_Floating-Point_Numbers.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Nis{
+    public static void main(String[] args)
+    {
+        Scanner n=new Scanner(System.in);
+        float a,b,c;
+        a=n.nextFloat();
+        b=n.nextFloat();
+        c=(a*b);
+        System.out.printf("%.2f",c);
+    }
+}
