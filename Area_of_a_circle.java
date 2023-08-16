cc5552ffc86bc3943b3304576f4a66aa296de00f
P Nishanth Reddy <119839756+nishanth-reddy-2005@users.noreply.github.com>
2023-08-16 14:47:33 +0530

    import java.util.*; public class Nis{     public static void main(String[] args)     {         Scanner n=new Scanner(System.in);         int a;         double d;         a=n.nextInt();         d=a*3.14*a;         System.out.printf("%.2f",d);     } }

---
+++ Area_of_a_circle.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Nis{
+    public static void main(String[] args)
+    {
+        Scanner n=new Scanner(System.in);
+        int a;
+        double d;
+        a=n.nextInt();
+        d=a*3.14*a;
+        System.out.printf("%.2f",d);
+    }
+}
