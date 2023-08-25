d914727c3b87617b3d8adf51c15b1c02cfc1bfc1
P Nishanth Reddy <119839756+nishanth-reddy-2005@users.noreply.github.com>
2023-08-25 13:31:19 +0530

    import java.util.*; public class Nis{     public static void main(String[] args)     {         Scanner n=new Scanner(System.in);         int a,i,s=0;         a=n.nextInt();         for(i=0;i<=a;i++)         {             s=s+i;         }         System.out.print(s);     } }

---
+++ Find_sum_of_natural_numbers_.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Nis{
+    public static void main(String[] args)
+    {
+        Scanner n=new Scanner(System.in);
+        int a,i,s=0;
+        a=n.nextInt();
+        for(i=0;i<=a;i++)
+        {
+            s=s+i;
+        }
+        System.out.print(s);
+    }
+}
