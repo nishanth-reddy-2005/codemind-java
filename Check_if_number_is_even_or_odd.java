b6ada838f8bc6bae5532d031321726b56240a4c7
P Nishanth Reddy <119839756+nishanth-reddy-2005@users.noreply.github.com>
2023-08-25 11:59:21 +0530

    import java.util.*; public class Nis{     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int n;         n=sc.nextInt();         if(n%2==0)         {             System.out.print("Even");         }         else         {             System.out.print("Odd");         }     } }

---
+++ Check_if_number_is_even_or_odd.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Nis{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int n;
+        n=sc.nextInt();
+        if(n%2==0)
+        {
+            System.out.print("Even");
+        }
+        else
+        {
+            System.out.print("Odd");
+        }
+    }
+}
