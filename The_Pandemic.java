79f1d1ed1f512a9c65a8a9769912baefcd4d5f18
P Nishanth Reddy <119839756+nishanth-reddy-2005@users.noreply.github.com>
2023-08-25 12:14:47 +0530

    import java.util.*; public class Nis{     public static void main(String[] args)     {         Scanner n=new Scanner(System.in);         int a;         a=n.nextInt();         if(a==1)         {             System.out.print("0");         }         else if(a==2)         {             System.out.print("1");         }         else         {             System.out.print("2");         }     } }

---
+++ The_Pandemic.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Nis{
+    public static void main(String[] args)
+    {
+        Scanner n=new Scanner(System.in);
+        int a;
+        a=n.nextInt();
+        if(a==1)
+        {
+            System.out.print("0");
+        }
+        else if(a==2)
+        {
+            System.out.print("1");
+        }
+        else
+        {
+            System.out.print("2");
+        }
+    }
+}
